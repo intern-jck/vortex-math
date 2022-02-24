5ef5011f5c0d2ccacf68e759469659e45f4c832d
Justin C Kirk <justin.c.kirk@gmail.com>
2022-02-24 16:57:59 -0500

    Colors and Interface
    Added colors to lines. Download button now changes inner lines to blue stroke color and outline to black for laser cutting.  Change the layout a built to make it more user friendly.

--- script.js
+++ script.js
@@ -68,28 +68,104 @@ function draw() {
 
 }
 
+// max color = radius
+// min color = 0
+
+//scale color of line based on length where length is some percentage of the color range?
+function remap(val, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (val - low1) / (high1 - low1);
+}
 
 function drawSvgLines(c, r, m, n, svg) {
     for (i = 0; i < n; i++) {
         let alpha = (i * m) % n;
         //console.log(alpha, i, (i * m));
+
         if (alpha != i) {
+
             //console.log(alpha);
             let x1 = c + r * Math.sin(i * 2 * Math.PI / n);
             let y1 = c - r * Math.cos(i * 2 * Math.PI / n);
             let x2 = c + r * Math.sin(m * i * 2 * Math.PI / n);
             let y2 = c - r * Math.cos(m * i * 2 * Math.PI / n);
+
             //console.log(x1, y1, x2, y2);
             let line = svgLine(x1, y1, x2, y2);
+
+            let lineLength = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2).toFixed(2);
+            let mappedLength = parseInt(remap(lineLength, 0, 2 * r, 0, n));
+
+            //let hexNumber = parseInt(remap(lineLength, 0, 2 * r, 0, 4096));
+
+            //let hexMod = hexNumber % 16;
+            //console.log("LINE VALS: ", lineLength, mappedLength, hexNumber, hexMod, hexMod.toString(16));
+
+            //console.log("LINE VALS: ", lineLength, mappedLength, hexColor);
+
+            let lineColor = getLineColor('#0000FF', '#FF0000', 0, n, mappedLength);
+            console.log("LINE VALS: ", lineLength, mappedLength, lineColor);
+            line.setAttribute('stroke', lineColor);
+
             svg.appendChild(line);
         }
     }
 }
 
+function hexToRGB(hex) {
+    let arr = /^#?([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/i.exec(hex);
+    let r = parseInt(arr[1], 16);
+    let g = parseInt(arr[2], 16);
+    let b = parseInt(arr[3], 16);
+    let rgb = {
+        'r': r,
+        'g': g,
+        'b': b,
+    }
+    //console.log(r, g, b);
+    // let rgb = "rgb(" + r + ',' + g + ',' + b + ")";
+    return rgb;
+}
+
+function getLineColor(color1, color2, min, max, val) {
+    // get start and end colors for range
+    let rgb1 = hexToRGB(color1);
+    let rgb2 = hexToRGB(color2);
+    //console.log(rgb1, rgb2);
+    // gradient fade
+    let fade = remap(val, min, max, 0, 1);
+
+    // get color ranges
+    let r = Math.abs(rgb2.r - rgb1.r);
+    let g = Math.abs(rgb2.g - rgb1.g);
+    let b = Math.abs(rgb2.b - rgb1.b);
+
+    // increment color by step
+    r = parseInt(r * fade - rgb1.r);
+    g = parseInt(g * fade - rgb1.g);
+    b = parseInt(b * fade - rgb1.b);
+    console.log(r, g, b);
+    // console.log(rgb1.b, rgb2.b, b, fade);
+
+    let rgb = "rgb(" + Math.abs(r) + ',' + Math.abs(g) + ',' + Math.abs(b) + ")";
+    return rgb;
+}
+
+// SVG Engine
 function downloadSVG() {
-    const svg = document.getElementById('svg-graph').innerHTML;
-    const blob = new Blob([svg.toString()]);
-    const element = document.createElement("a");
+    // get the svg
+    let svg = document.getElementById('svg-graph');
+    // get ellipse
+    let outline = svg.getElementsByTagName('ellipse');
+    // get lines
+    outline.setAttribute('stroke', 'black');
+    let lines = svg.getElementsByTagName('line');
+    //console.log(lines.length);
+    for (let line in lines) {
+        line.setAttribute('stroke', 'blue');
+    }
+    let svgFile = svg.innerHTML;
+    let blob = new Blob([svgFile.toString()]);
+    let element = document.createElement("a");
     element.download = "svg-coaster.svg";
     element.href = window.URL.createObjectURL(blob);
     element.click();
@@ -111,7 +187,7 @@ function svgLine(x1, y1, x2, y2) {
     myLine.setAttribute("y1", parseInt(y1));
     myLine.setAttribute("x2", parseInt(x2));
     myLine.setAttribute("y2", parseInt(y2));
-    myLine.setAttribute('stroke', 'blue');
+    // myLine.setAttribute('stroke', c);
     return myLine;
 }
 
